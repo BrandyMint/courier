50334d512bbbeead5361fca8ce88469b49cf97b0
Aleksandr Ivanov <radanisk@ya.ru>
2012-05-15 10:30:21 +0400

    [#26845937] Корректировка страницы отписки

--- app/controllers/courier/subscribers_controller.rb
+++ app/controllers/courier/subscribers_controller.rb
@@ -10,4 +10,26 @@ class Courier::SubscribersController < ApplicationController
 
     respond_with @subscriber
   end
+
+  def deactivate
+    notice = nil
+
+    @subscriber = Courier::Subscriber.find_by_token(params[:id])
+    if @subscriber
+      if @subscriber.is_active
+        notice = "Вы отписаны от рассылки: #{@subscriber.title}."
+        login_user @subscriber.user
+
+        @subscriber.deactivate
+      else
+        login_user @subscriber.user
+
+        notice = "Вы не подписаны на рассылку: #{@subscriber.title}."
+      end
+
+      redirect_to urls.personal_subscriptions_url, notice: notice
+    else
+      render :show
+    end
+  end
 end
